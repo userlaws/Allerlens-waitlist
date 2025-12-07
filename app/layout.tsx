5aa1b0357e1c2ad62be392dbdbd3a66c4f9bcaf9
Imanol Aracena <laws@mac.mynetworksettings.com>
2025-12-07 00:33:19 -0500

    fixed the tab icon.

--- app/layout.tsx
+++ app/layout.tsx
@@ -73,21 +73,8 @@ export const metadata: Metadata = {
     },
   },
   icons: {
-    icon: [
-      {
-        url: "/icon-light-32x32.png",
-        media: "(prefers-color-scheme: light)",
-      },
-      {
-        url: "/icon-dark-32x32.png",
-        media: "(prefers-color-scheme: dark)",
-      },
-      {
-        url: "/icon.svg",
-        type: "image/svg+xml",
-      },
-    ],
-    apple: "/apple-icon.png",
+    icon: "/icon.png",
+    apple: "/icon.png",
   },
   manifest: "/manifest.json",
 }
